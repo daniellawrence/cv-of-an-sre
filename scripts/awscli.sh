6c6d48d23eec316d113e28caffa9f9303f2a027d
Daniel Lawrence <github@dansysadm.com>
2026-03-06 13:11:22 +1100

    chore(clean): fresh start

--- scripts/awscli.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Pass args into aws-cli that is preconfigured to use the localstack instance.
-
-docker run -v ${PWD}:/mnt -it --rm --network cv_default \
-	-e AWS_ACCESS_KEY_ID=test \
-	-e AWS_SECRET_ACCESS_KEY=test \
-	-e AWS_DEFAULT_REGION=ap-southeast-2 \
-	amazon/aws-cli:latest --endpoint-url=http://localstack:4566 $@
