50d204df5872f6d6686e0f13d5b537de5960c3d3
Daniel Lawrence <github@dansysadm.com>
2026-03-07 11:56:19 +1100

    chore(infra/argo): enable ingress

--- scripts/1_argo_init.sh
+++ scripts/1_argo_init.sh
@@ -3,11 +3,11 @@ source $(dirname $0)/lib.sh
 cd $GIT_ROOT
 bin/helmfile \
     --helm-binary $GIT_ROOT/bin/helm \
-    --file argocd/helmfile.yaml \
+    --file apps/infra/argocd/helmfile.yaml \
     template --output-dir-template artefacts
 bin/helmfile \
     --helm-binary $GIT_ROOT/bin/helm \
-    --file argocd/helmfile.yaml \
+    --file apps/infra/helmfile.yaml \
     sync
 
 ADMIN_PASSWORD=$(./scripts/kubectl.sh -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
